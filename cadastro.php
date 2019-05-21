c8e2c5bf52ddcb2ce3d2b89bbc71a705bf6e67fa
Jovio <jovioluiz@hotmail.com>
2019-05-20 22:17:23 -0300

    aula 20/05

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,33 @@
+<?php
+if (isset($_POST['cadastrar'])) {
+	$nome = $_POST['nome'];
+	$tipo = $_POST['tipo'];
+	$local = $_POST['local'];
+	$descricao = $_POST['descricao'];
+	$totalIngressos = $_POST['totalIngressos'];
+	$valor = $_POST['valor'];
+	$data = $_POST['data'];
+	$hora = $_POST['hora'];
+	$venda = $_POST['venda'];
+
+	//insert
+	$sql = "insert into evento (nome, tipo,	local, descricao, totalIngressos, valor, data, hora, vendaAberta) values 
+	('$nome', $tipo, '$local', '$descricao', $totalIngressos, $valor, '$data', '$hora', $venda)";
+
+	//echo $sql;
+
+	include_once "includes/conexao.php";
+	if(mysqli_query($conexao, $sql)){
+		echo "Cadastro Realizado com Sucesso";
+	} else{
+		echo "Ocorreu em erro no cadastro";
+		echo mysqli_error($conexao);
+	}
+	echo "<p><a href=\"cadastro.php\">Voltar</a></p>";
+	die();//interrompe o script
+}
+?>
+
 <h1>Cadastro de eventos</h1>
 <form action="" method="post">
 	<div>
@@ -18,19 +48,19 @@
 		</label>
 	</div>
 	<div>
-		<label>Local: <input type="text" name="local_evento" maxlength="100" size="50"></label>
+		<label>Local: <input type="text" name="local" maxlength="100" size="50"></label>
 	</div>
 	<div>
 		<label>Descrição: <textarea cols="50" rows="10" name="descricao"></textarea></label>
 	</div>
 	<div>
-		<label>Total de ingressos: <input type="number" name="total_ingressos" min="0" value="0"></label>
+		<label>Total de ingressos: <input type="number" name="totalIngressos" min="0" value="0"></label>
 	</div>
 	<div>
 		<label>Valor: <input type="number" name="valor" min="0" value="0"></label>
 	</div>
 	<div>
-		<label>Data do evento: <input type="date" name="data_evento"></label>
+		<label>Data do evento: <input type="date" name="data"></label>
 	</div>
 	<div>
 		<label>Hora do evento: <input type="time" name="hora"></label>
