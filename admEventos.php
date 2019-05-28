25f8c61526355c76e74ff0ee83549472c43d8355
Jovioluiz <jovioluiz@hotmail.com>
2019-05-27 22:16:06 -0300

    Update admEventos.php

--- admEventos.php
+++ admEventos.php
@@ -1,23 +1,22 @@
 <?php 
 include_once "includes/conexao.php";
-$sql = "select nome, tipo, date_format(data, '%d/%m/%Y') as data, ((totalVendidos/totalIngressos) * 100) as vendidos, vendaAberta from evento order by data desc";
+include_once "includes/functions.php";
+$sql = "select nome, tipo, date_format(data, '%d/%m/%Y') as data, ((totalVendidos/totalIngressos) * 100) as vendidos, vendaAberta, local from evento order by data desc";
 $resultado = mysqli_query($conexao, $sql);
-
 ?>
-
-
 <h1>Administração de Eventos</h1>
+<p><a href="cadastro.php"></a>Cadastrar novo Eventos</p>
 <?php
 if (mysqli_num_rows($resultado) == 0) {
 	echo "<p>Nenhum Evento Cadastrado</p>";
 } else{
-
 	?>
 	<table border="1">
 		<tr>
 			<th>Nome</th>
 			<th>Tipo</th>
 			<th>Data</th>
+			<th>Local</th>
 			<th>Vendidos</th>
 			<th>Venda Aberta</th>
 		</tr>
@@ -26,10 +25,11 @@ if (mysqli_num_rows($resultado) == 0) {
 			?>
 			<tr>
 				<td><?php echo $evento['nome'];?></td>
-				<td><?=$evento['tipo']?></td>
+				<td><?=exibeTipo($evento['tipo'])?></td>
 				<td><?=$evento['data']?></td>
-				<td><?=$evento['vendidos']?></td>
-				<td><?=$evento['vendaAberta']?></td>
+				<td><?=$evento['local']?></td>
+				<td><?=exibePorcentagem($evento['vendidos'])?></td>
+				<td><?=exibeVendaAberta($evento['vendaAberta'])?></td>
 			</tr>
 			<?php
 		}
